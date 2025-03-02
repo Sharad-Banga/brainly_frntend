3cd9cee821086a184b6f52d4fcf29e5317a544b8
Sharad Banga <sharadiit03@gmail.com>
2025-03-02 21:31:12 +0530

    update

--- brainly-f/src/App.tsx
+++ brainly-f/src/App.tsx
@@ -1,19 +1,20 @@
 
 import './App.css'
-import { Button } from './components/button'
-import { PlusIcon } from './icons/PlusIcon';
+import { Card } from './components/card'
+// import { Button } from './components/button'
+// import { PlusIcon } from './icons/PlusIcon';
  function App() {
 
   return (
     <>
-      <Button variant="primary" size="lg" onClick={()=>{console.log("chl pea");
-      }} text="ss" startIcon={<PlusIcon/>} />
+      {/* <Button variant="primary" size="lg" onClick={()=>{console.log("chl pea");
+      }} text="ss" startIcon={<PlusIcon/>} /> */}
 
      {/*  <Button variant="secondary" size="md" onClick={()=>{console.log("chl pea");
       }} text="ss" startIcon={<PlusIcon/>} /> */}
 
       
-
+      <Card />
  
     </>
   )
--- brainly-f/src/components/card.tsx
+++ brainly-f/src/components/card.tsx
@@ -3,7 +3,9 @@
 export const Card = () =>{
   return(
     <>
-        
+        <div className="bg-red-600 min-h-60 w-44">
+          
+        </div>
     </>
   )
 }
