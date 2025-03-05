2d36532a9d201fa7111446bdb843d1ac8e1ea3a9
Sharad Banga <sharadiit03@gmail.com>
2025-03-05 20:21:25 +0530

    update

--- brainly-f/src/App.tsx
+++ brainly-f/src/App.tsx
@@ -1,17 +1,18 @@
 
 import './App.css'
-import { Card } from './components/card'
-import { Button } from './components/button'
+import { Card } from './components/Card'
+import { Button } from './components/Button'
 import { PlusIcon } from './icons/PlusIcon';
+import { ShareIcon } from './icons/ShareIcon';
  function App() {
 
   return (
     <>
-      <Button variant="primary" size="lg" onClick={()=>{console.log("chl pea");
-      }} text="ss" startIcon={<PlusIcon/>} /> 
+      <Button variant="primary" size="md" onClick={()=>{console.log("chl pea");
+      }} text="Add Content" startIcon={<PlusIcon/>} /> 
 
-      <Button variant="secondary" size="md" onClick={()=>{console.log("chl pea");
-      }} text="ss" startIcon={<PlusIcon/>} />
+      <Button variant="secondary" size="sm" onClick={()=>{console.log("chl pea");
+      }} text="Share" startIcon={<ShareIcon/>} />
 
       <br />
       
--- brainly-f/src/components/card.tsx
+++ brainly-f/src/components/card.tsx
@@ -1,10 +1,54 @@
+import { ShareIcon } from "../icons/ShareIcon"
 
 
 export const Card = () =>{
   return(
     <>
-        <div className="bg-red-600 min-h-60 w-44">
-          
+        <div className="p-4 bg-red rounded-md  border-gray-300 max-w-72 border-2">
+                  <div className="flex justify-between flex-row items-center ">
+                            <div className="flex">
+                              <div className="pr-2">
+                                <ShareIcon/>
+                              </div>
+                              
+                              Project Ideas
+                            </div>
+
+                              <div className="flex">
+
+                                  <div className="pr-2 text-gray-500">
+                                    <ShareIcon/>
+                                  </div>
+                                  <div className="text-gray-500">
+                                    <ShareIcon/>
+                                  </div>
+
+                              </div>
+                  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                  
         </div>
     </>
   )
