937b44fdd06ccd16f8537ae3fea527f95ce0ceb2
Sharad Banga <sharadiit03@gmail.com>
2025-03-05 19:32:00 +0530

    update

--- brainly-f/src/App.tsx
+++ brainly-f/src/App.tsx
@@ -1,18 +1,19 @@
 
 import './App.css'
 import { Card } from './components/card'
-// import { Button } from './components/button'
-// import { PlusIcon } from './icons/PlusIcon';
+import { Button } from './components/button'
+import { PlusIcon } from './icons/PlusIcon';
  function App() {
 
   return (
     <>
-      {/* <Button variant="primary" size="lg" onClick={()=>{console.log("chl pea");
-      }} text="ss" startIcon={<PlusIcon/>} /> */}
+      <Button variant="primary" size="lg" onClick={()=>{console.log("chl pea");
+      }} text="ss" startIcon={<PlusIcon/>} /> 
 
-     {/*  <Button variant="secondary" size="md" onClick={()=>{console.log("chl pea");
-      }} text="ss" startIcon={<PlusIcon/>} /> */}
+      <Button variant="secondary" size="md" onClick={()=>{console.log("chl pea");
+      }} text="ss" startIcon={<PlusIcon/>} />
 
+      <br />
       
       <Card />
  
