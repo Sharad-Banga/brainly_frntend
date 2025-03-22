88766da86c9e591371ce76144874282add4810f7
Sharad Banga <sharadiit03@gmail.com>
2025-03-22 07:04:48 +0530

    update

--- brainly-f/vite.config.ts
+++ brainly-f/vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-
-
-
 export default defineConfig({
-  base: "./", // or "/your-repo-name/"
-});
+  plugins: [react()],
+})
